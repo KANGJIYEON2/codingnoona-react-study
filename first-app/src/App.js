26bcb0848f9655e33667c1ab8a22121e262ec79b
KANGJIYEON2 <127065529+KANGJIYEON2@users.noreply.github.com>
2024-03-20 20:46:19 +0900

    update_day2

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -18,17 +18,44 @@ const choice = {
 };
 
 function App() {
-  let [userSelect, setUserSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  //숙제
+  //1.이기고지고 반대로 나오기
+  //2.승패결과를 보고 테두리가 바꿔지게(초록, 빨강)
+  const judgement = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie";
+    } else if (user.name === "Rock")
+      return computer.name === "Scissors" ? "win" : "lose";
+    else if (user.name === "Scissors")
+      return computer.name === "Paper" ? "win" : "lose";
+    else if (user.name === "Paper")
+      return computer.name === "Rock" ? "win" : "lose";
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice); //객체에 키값만 뽑아서 어레이로 만듦
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
   };
 
   return (
     <div>
+      <h2>가위바위보게임</h2>
       <div className="main">
-        <Box title="You" item={userSelect} />
-        {/*<Box title="Computer" /> */}
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result} />
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
