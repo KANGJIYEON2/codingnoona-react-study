190182971a6bb98928d5f68b4fa981d748b58f5d
KANGJIYEON2 <127065529+KANGJIYEON2@users.noreply.github.com>
2024-04-07 22:02:43 +0900

    update

--- mymovie-app/src/pages/Homepage/Homepage.jsx
+++ mymovie-app/src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Banner from "./components/Banner/Banner";
-import PupularMovieSlide from "./components/PupularMovieSlide/PupularMovieSlide";
 const Homepage = () => {
   return (
     <div>
       <Banner />
-      <PupularMovieSlide />
     </div>
   );
 };
